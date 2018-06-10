83ceab3edf36d3a02fe519f9cba1d1b98ceda6a3
landresfernamdez <landresf3638@gmail.com>
2018-06-10 16:27:57 -0600

    Integracion del login

--- AppExpress/components/Home.js
+++ AppExpress/components/Home.js
@@ -3,110 +3,10 @@ import Modal from 'react-native-modalbox'
 import axios from 'axios';
 import { ListView, Text, View, Image, StyleSheet,TextInput, ActivityIndicator, Alert,
 Dimensions,Platform,TouchableHighlight,TouchableOpacity} from 'react-native';
-import { Button } from 'react-native-elements';
+import { Button,FormLabel, FormInput } from 'react-native-elements';
 import NumericInput,{ calcSize } from 'react-native-numeric-input';
 var screen=Dimensions.get('window');
-const styles = StyleSheet.create({
-  containerList: {
-    flex: 1,
-    marginTop: 20,
-    flexDirection: 'column'
-  },container1: {
-    flex: 1,
-    marginTop: 0,
-    flexDirection: 'row',
-  },containerHeader: {
-    marginTop: 0,
-    marginLeft: 0,
-    flexDirection: 'row',
-    height:20
-  },container2:{
-    flex: 1,
-    marginTop: 10,
-    flexDirection: 'column',
-  },modalImg: {
-    flex: 1,
-    justifyContent: 'center',
-    marginTop: 10,
-    flexDirection: 'column',
-  },modalImg1: {
-    marginTop: 10,
-    flexDirection: 'row',
-  },elemento:{
-    flexDirection: 'column',
-  },modal1: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },modal3: {
-    flex: 1,
-    flexDirection: 'column'
-  },
-  img:{
-    marginTop: 10,
-    marginLeft: 0,
-    width: 193,
-    height: 110,
-  },
-  img1:{
-    marginTop: 10,
-    marginLeft: 0,
-    width: 30,
-    height: 30,
-    flexDirection: 'column',
-  },img_car:{
-    marginTop: 10,
-    marginLeft: 0,
-    width: 50,
-    height: 50,
-  },modal:{
-    justifyContent:'center',
-    borderRadius: Platform.OS==='android'?30:0,
-    shadowRadius: 10,
-    width:screen.width-80,
-    height:280
-  },modal2:{
-    justifyContent:'center',
-    borderRadius: Platform.OS==='android'?30:0,
-    shadowRadius: 5,
-    width:screen.width-10,
-    height:screen.height-10
-  },botonModal:{
-    marginBottom:0
-  },botonHeader:{
-    marginRight:0,
-    backgroundColor: '#859a9b',
-    borderRadius: 5,
-    padding: 10,
-    width:45,
-    height:45,
-    marginBottom: 20,
-    shadowColor: '#303838',
-    shadowOffset: { width: 0, height: 5 },
-    shadowRadius: 10,
-    shadowOpacity: 0.35,
-  },botonHeader1:{
-    marginRight:0,
-    backgroundColor: '#859a9b',
-    borderRadius: 5,
-    padding: 10,
-    width:30,
-    height:30,
-    marginBottom: 20,
-    shadowColor: '#303838',
-    shadowOffset: { width: 0, height: 5 },
-    shadowRadius: 10,
-    shadowOpacity: 0.35,
-  },icon: {
-    width: 24,
-    height: 24,
-  },TextInputStyleClass:{
-    backgroundColor:'#8AE2FF',
-    height:45,
-    width:300
-  }
-});
+
 export default class Home extends React.Component {
   static navigationOptions = {
     drawerLabel: 'Inicio',
@@ -117,17 +17,21 @@ export default class Home extends React.Component {
       />
     )
   };
-  constructor(props) {
+  constructor(props){
     super(props);
     this.bandera=true;
     this.interval= setInterval(() => {
       this.componentDidMount();
     }, 25000);
+    this.sesion=false;
     this.arrayholder=[{'nombre':"Ensalada",'imagen':"https://mobile-cdn.123rf.com/300wm/serezniy/serezniy1110/serezniy111000110/10752709-sabrosa-ensalada-griega-en-recipiente-transparente-aislado-en-blanco.jpg?ver=6",'precio':1000,'ingredientes':'Tomate,lechuga,pepino,limon','cantidadcalorias':'5'}];
     let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.arrayholder1=[];
     let ds1 = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-    this.state = { 
+    this.state = {
+      userName:'',
+      password:'', 
+      modalVisibleLoginVisible:false,
       value:0,
       dataSource:ds.cloneWithRows(this.arrayholder),
       dataSource1:ds.cloneWithRows(this.arrayholder1),
@@ -141,7 +45,6 @@ export default class Home extends React.Component {
   }
   componentDidMount(){
     console.log("Entro a prueba");
-    //this.setTimeout(() => {
       axios.get('https://guarded-eyrie-96688.herokuapp.com/seleccionarProductos')
       .then(response=>{
         let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
@@ -156,7 +59,6 @@ export default class Home extends React.Component {
       }).catch(function (error) {
         return error.data
     })
-    //}, 500);
   }
   SearchFilterFunction(text){
     const newData = this.arrayholder.filter(function(item){
@@ -201,6 +103,30 @@ setStateModalCarrito(){
     'modalCarritoVisible':false
                })
 
+}
+BuyCar(){
+  this.setState({
+      modalVisibleLoginVisible:true
+  });
+}
+validarLogin(){
+  axios.post('http://172.24.113.47:5000/iniciarSesionCliente',{
+    correo:this.state.userName,
+    contrasena:this.state.password
+    })
+    .then(result => {
+        console.log(result);
+        if(result.data.success==true){
+            console.log("Usuario Invalido")
+        }
+        else{
+            console.log("Usuario valido")
+            }
+        }
+    )
+    .catch(error=> {
+    console.log(error);
+    });
 }
 //Se encarga de modificar la cantidad del producto en el carrito
 agregarUnidades(cantidad,nombre){
@@ -304,6 +230,7 @@ addShoppinCar(producto){
                           </View>
                             }
                       />
+                       {/*Este es el modal de mostrar detalles de un producto*/}
                       <Modal
                         style={styles.modal}
                         position='center'
@@ -326,6 +253,7 @@ addShoppinCar(producto){
                           </View>
                         </View>
                       </Modal>
+                      {/*Este es el modal del carrito de compras*/}
                       <Modal
                         style={styles.modal2}
                         position='center'
@@ -338,6 +266,9 @@ addShoppinCar(producto){
                           <Text style={styles.elemento}>Precio  </Text>
                           <Text style={styles.elemento}>Cantidad  </Text>
                           <Text style={styles.elemento}>Subtotal</Text>
+                          <TouchableOpacity style={styles.botonHeaderModalCar} onPress={this.setStateModalCarrito.bind(this)}>
+                                <Image source={require("../assets/images/close.png")}/>
+                        </TouchableOpacity>
                         </View>
                         <ListView
                         style={styles.containerList}
@@ -357,10 +288,167 @@ addShoppinCar(producto){
                             </TouchableOpacity>
                           </View>
                             }
-                      /><Button style={styles.botonModal} title="Cerrar" color="black" onPress={this.setStateModalCarrito.bind(this)}></Button>
+                      />
+                      <TouchableOpacity style={styles.botonHeader} onPress={this.BuyCar.bind(this)}>
+                                <Image source={require("../assets/images/payment.png")}/>
+                        </TouchableOpacity>
+                        </View>
+                      </Modal>
+                      {/*Este es el modal del Login*/}
+                      <Modal
+                        style={styles.modal}
+                        position='center'
+                        backdrop={true}
+                        isOpen={this.state.modalVisibleLoginVisible}>
+                        <View style={styles.containerLogin}>
+                            <Text >Bienvenido a InstantFoodExpress</Text>
+                            <FormLabel>Digite su usuario</FormLabel>
+                            <TextInput style={styles.inputLogin}
+                            underlineColorAndroid='transparent'
+                            placeholder="Digite su usuario"
+                              onChangeText={text => this.setState({ userName: text })}
+                            />
+                            <FormLabel>Contraseña</FormLabel>
+                            <TextInput style={styles.inputLogin}
+                              underlineColorAndroid='transparent'
+                              placeholder="Digite su contraseña"
+                              onChangeText={text => this.setState({ password: text })}
+                            />
+                            <Button 
+                              onPress={this.validarLogin.bind(this)}
+                              buttonStyle={[{ marginBottom: 5, marginTop: 5 }]}
+                              title="Login"
+                            />
                         </View>
                       </Modal>
               </View>
     );
   }
 }
+const styles = StyleSheet.create({
+  containerLogin:{
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    flexDirection:'column'
+  },
+  inputLogin: {
+    borderRadius: 5,
+    backgroundColor:'#8AE2FF',
+    width: 200,
+    height: 44,
+    borderWidth: 1,
+    borderColor: 'black'
+  },
+  containerList: {
+    flex: 1,
+    marginTop: 20,
+    flexDirection: 'column'
+  },container1: {
+    flex: 1,
+    marginTop: 0,
+    flexDirection: 'row',
+  },containerHeader: {
+    marginTop: 0,
+    marginLeft: 0,
+    flexDirection: 'row',
+    height:20
+  },container2:{
+    flex: 1,
+    marginTop: 10,
+    flexDirection: 'column',
+  },modalImg: {
+    flex: 1,
+    justifyContent: 'center',
+    marginTop: 10,
+    flexDirection: 'column',
+  },modalImg1: {
+    marginTop: 10,
+    flexDirection: 'row',
+  },elemento:{
+    flexDirection: 'column',
+  },modal1: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },modal3: {
+    flex: 1,
+    flexDirection: 'column'
+  },
+  img:{
+    marginTop: 10,
+    marginLeft: 0,
+    width: 193,
+    height: 110,
+  },
+  img1:{
+    marginTop: 10,
+    marginLeft: 0,
+    width: 30,
+    height: 30,
+    flexDirection: 'column',
+  },img_car:{
+    marginTop: 10,
+    marginLeft: 0,
+    width: 50,
+    height: 50,
+  },modal:{
+    justifyContent:'center',
+    borderRadius: Platform.OS==='android'?30:0,
+    shadowRadius: 10,
+    width:screen.width-80,
+    height:280
+  },modal2:{
+    justifyContent:'center',
+    borderRadius: Platform.OS==='android'?30:0,
+    shadowRadius: 5,
+    width:screen.width-10,
+    height:screen.height-10
+  },botonModal:{
+    marginBottom:0
+  },botonHeader:{
+    marginRight:0,
+    backgroundColor: '#859a9b',
+    borderRadius: 5,
+    padding: 10,
+    width:45,
+    height:45,
+    marginBottom: 20,
+    shadowColor: '#303838',
+    shadowOffset: { width: 0, height: 5 },
+    shadowRadius: 10,
+    shadowOpacity: 0.35,
+  },botonHeaderModalCar:{
+    marginRight:0,
+    backgroundColor: '#859a9b',
+    borderRadius: 5,
+    width:35,
+    height:45,
+    marginBottom: 20,
+    shadowColor: '#303838',
+    shadowOffset: { width: 0, height: 5 },
+    shadowRadius: 10,
+    shadowOpacity: 0.35,
+    justifyContent:'center',
+  },botonHeader1:{
+    marginRight:0,
+    backgroundColor: '#859a9b',
+    borderRadius: 5,
+    padding: 10,
+    width:30,
+    height:30,
+    marginBottom: 20,
+    shadowColor: '#303838',
+    shadowOffset: { width: 0, height: 5 },
+    shadowRadius: 10,
+    shadowOpacity: 0.35,
+  },icon: {
+    width: 24,
+    height: 24,
+  },TextInputStyleClass:{
+    backgroundColor:'#8AE2FF',
+    height:45,
+    width:300
+  }
+});
